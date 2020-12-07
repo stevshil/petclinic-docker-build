35c4621736dc999a7f2276dd25d97f247d32768e
Steve Shilling <stevshil@users.noreply.github.com>
2020-12-07 08:37:42 +0000

    correcting tags

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -12,17 +12,16 @@ resource "aws_autoscaling_group" "petclinic" {
 
   tags = concat([
     {
-      "key" = "Name"
-      "value" = "${var.project}-${terraform.workspace}-jenkins"
+      "key"                 = "Name"
+      "value"               = "${var.project}-${terraform.workspace}-jenkins"
       "propagate_at_launch" = true
     },
     {
-      "key" = "Environment"
-      "value" = terraform.workspace
+      "key"                 = "Environment"
+      "value"               = terraform.workspace
       "propagate_at_launch" = true
     }
-
-  }
+  ])
 }
 
 resource "aws_autoscaling_attachment" "petclinic" {
